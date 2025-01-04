6e53866f053883d90a30b818851cbc949bd82000
hagaitski <i.haga629@gmail.com>
2025-01-04 22:15:38 +0900

    send

---
+++ send-error-log-to-discord.ts
@@ -0,0 +1,59 @@
+import { spawn } from "node:child_process";
+import { setTimeout } from "node:timers/promises";
+
+const WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
+const VERCEL_TOKEN = process.env.VERCEL_TOKEN;
+
+if (!WEBHOOK_URL) {
+  console.error("DISCORD_WEBHOOK_URL is not set");
+  process.exit(1);
+}
+
+if (!VERCEL_TOKEN) {
+  console.error("VERCEL_TOKEN is not set");
+  process.exit(1);
+}
+
+const safeJsonParse = (str: string) => {
+  try {
+    return JSON.parse(str);
+  } catch (e) {
+    return null;
+  }
+};
+
+const spawnProcess = spawn("npx", [
+  "vercel",
+  "logs",
+  "--token",
+  VERCEL_TOKEN,
+  "-j",
+  "https://iesona.com",
+]);
+spawnProcess.stdout.on("data", (data) => {
+  const log = safeJsonParse(data.toString());
+  if (log) {
+    console.log(log);
+  }
+  if (log && log.level === "info") {
+    console.log("Error found");
+    fetch(WEBHOOK_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: data.toString(),
+      }),
+    });
+  }
+})
+
+spawnProcess.stderr.on("data", (data) => {
+  console.error(data.toString());
+})
+
+await setTimeout(1000 * 10);
+
+spawnProcess.kill();
+console.log("Done");
